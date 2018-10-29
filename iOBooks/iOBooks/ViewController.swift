2dcf7c7ab9560e95b59fcb2ed591064ce97b35f8
OmegaWulf <nikcxz@live.com>
2018-10-29 12:38:39 -0400

    Created Fresh Project

---
+++ iOBooks/iOBooks/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOBooks
+//
+//  Created by Nikita Thomas on 10/29/18.
+//  Copyright © 2018 Nikita Thomas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
