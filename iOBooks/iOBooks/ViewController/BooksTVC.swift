89bba35ae9d6e7629b30f2398472dc903ddd5cbb
OmegaWulf <nikcxz@live.com>
2018-10-29 20:00:56 -0400

    Removing books from Bookshelves

--- iOBooks/iOBooks/ViewController/BooksTVC.swift
+++ iOBooks/iOBooks/ViewController/BooksTVC.swift
@@ -51,25 +51,20 @@ class BooksTVC: UITableViewController {
     }
     
     
-    /*
-     // Override to support conditional editing of the table view.
-     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-     // Return false if you do not want the specified item to be editable.
-     return true
-     }
-     */
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
     
-    /*
-     // Override to support editing the table view.
-     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-     if editingStyle == .delete {
-     // Delete the row from the data source
-     tableView.deleteRows(at: [indexPath], with: .fade)
-     } else if editingStyle == .insert {
-     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-     }
-     }
-     */
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            guard var book = bookshelf?.books[indexPath.row] else {return}
+            let replacedArray = book.bookshelves.filter({ $0.name != bookshelf?.name })
+            book.bookshelves = replacedArray
+            
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
     
     /*
      // Override to support rearranging the table view.
@@ -85,8 +80,6 @@ class BooksTVC: UITableViewController {
      return true
      }
      */
-    
-    
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard let destination = segue.destination as? DetailVC else {return}
