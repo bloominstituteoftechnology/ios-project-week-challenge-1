7fc9e06f1252aa2c092bdb884036390bccc287e5
yzhukovs <yzhukovs@bumnetworks.com>
2018-10-30 22:23:15 -0400

    deleted some things

--- Books-ProjectWeek/Books-ProjectWeek/Controllers/DetailViewController.swift
+++ Books-ProjectWeek/Books-ProjectWeek/Controllers/DetailViewController.swift
@@ -9,27 +9,15 @@
 import UIKit
 
 class DetailViewController: UIViewController, UIPickerViewDelegate {
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        // Do any additional setup after loading the view.
-    }
-    
-    
-    
-    var newBook: Volume?{
+    var newBook: Volume? {
         didSet{
             if let book = newBook {
                 RealFirebase().storeBooks(volume: book, completion: {
-                    NSLog("\($0)") })
+                    NSLog("storeBooks \($0)")
+                })
             }
-            
         }
-        
     }
-    
-    
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -42,59 +30,33 @@ class DetailViewController: UIViewController, UIPickerViewDelegate {
                 self.bookImage?.image = image
             }
         }
-        author.text = nb.volumeInfo.authors?.joined(separator:" , ")
+        author.text = nb.volumeInfo.authors?.joined(separator:", ")
         descriptionBook.text = nb.volumeInfo.description
         RealFirebase().isRead(id:nb.id) {
             (rb:ReadBook?, error: Error? ) in
             if let error = error {
-                NSLog("is read error \(error)")
+                NSLog("isRead \(error)")
                 return
             }
             guard let rb = rb else {return}
             DispatchQueue.main.async {
                 self.isRead.isOn = rb.read
-                
             }
         }
     }
     
-    
     @IBOutlet weak var descriptionBook: UITextView!
-    
-    
     @IBOutlet weak var bookName: UILabel!
-    
     @IBOutlet weak var author: UILabel!
-    
     @IBOutlet weak var bookImage: UIImageView!
-    
-    
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destination.
-     // Pass the selected object to the new view controller.
-     }
-     */
-    
-    
     @IBOutlet weak var isRead: UISwitch!
-    
+    @IBOutlet weak var booksPicker: UIPickerView!
+
     @IBAction func read(_ sender: UISwitch) {
         RealFirebase().markRead(id:(self.newBook?.id)!, read: sender.isOn) {
             NSLog("switch broken \($0)")
         }
-        
     }
-    
-    
-    
-    @IBOutlet weak var booksPicker: UIPickerView!
-    
-    
-    
     
     @IBAction func addToShelf(_ sender: Any) {
         let alertController: UIAlertController = UIAlertController(title: "Add Book", message: "Please enter Book name", preferredStyle: .alert)
@@ -118,11 +80,5 @@ class DetailViewController: UIViewController, UIPickerViewDelegate {
         
         //Present the AlertController
         present(alertController, animated: true, completion: nil)
-        
-        
-        
     }
-    
-    
-    
 }
